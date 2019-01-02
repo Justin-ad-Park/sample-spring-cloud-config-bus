423c523789af1a70ab15ff5304485cab5241096f
cjloveu <justin@11street.co.th>
2019-01-02 16:40:03 +0900

    Spring config - AMQP(RabbitMQ)를 이용한 변경 이벤트

--- sample-client-service/src/main/java/pl/piomin/service/sample/client/ClientApplication.java
+++ sample-client-service/src/main/java/pl/piomin/service/sample/client/ClientApplication.java
@@ -1,19 +1,21 @@
 package pl.piomin.service.sample.client;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
 @EnableDiscoveryClient
+@SpringBootApplication
 public class ClientApplication {
 
 	/**
-	 * -- 직접 트리거링 방식 --
+	 * -- How to Test 1. Triggering via client directly  --
 	 * 1.
 	 * http://localhost:8081/ping
 	 *
-	 * 2. zone1의 설정 변경 커밋 후
+	 * 2. Modify and commit configuration file
 	 * https://github.com/Justin-ad-Park/spring-cloud-config-repo/blob/master/client-service-zone1.yml
 	 *
 	 * 3. 프로퍼티 refresh
@@ -21,6 +23,18 @@ public class ClientApplication {
 	 *
 	 * 4. 변경 확인
 	 * http://localhost:8081/ping
+	 *
+	 *
+	 *  -- How to Test 2. Triggering via config server  --
+	 *
+	 *  1. Modify and commit configuration file
+	 *  https://github.com/Justin-ad-Park/spring-cloud-config-repo/blob/master/client-service-zone1.yml
+	 *
+	 *  2. Call command as below
+	 *  curl -H "X-Github-Event: push" -H "Content-Type: application/json" -X POST -d '{"commits": [{"modified": ["client-service-zone1.yml"]}]}' http://localhost:8889/monitor
+	 *
+	 *  3. 변경 확인
+	 * 	http://localhost:8081/ping
 	 *
 	 */
 	public static void main(String[] args) {
--- sample-config-service/src/main/java/pl/piomin/service/sample/config/ConfigApplication.java
+++ sample-config-service/src/main/java/pl/piomin/service/sample/config/ConfigApplication.java
@@ -2,7 +2,9 @@ package pl.piomin.service.sample.config;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.config.monitor.GithubPropertyPathNotificationExtractor;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableConfigServer
@@ -12,4 +14,8 @@ public class ConfigApplication {
 		new SpringApplicationBuilder(ConfigApplication.class).web(true).run(args);
 	}
 
+	@Bean
+	public GithubPropertyPathNotificationExtractor githubPropertyPathNotificationExtractor() {
+		return new GithubPropertyPathNotificationExtractor();
+	}
 }
