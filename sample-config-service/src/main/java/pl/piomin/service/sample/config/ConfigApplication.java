7c50badd41ac8100a71daf5a92cc07890ed40512
cjloveu <justin@11street.co.th>
2019-01-07 14:57:33 +0900

    Update "how to run rabbitMP"

--- sample-config-service/src/main/java/pl/piomin/service/sample/config/ConfigApplication.java
+++ sample-config-service/src/main/java/pl/piomin/service/sample/config/ConfigApplication.java
@@ -6,6 +6,23 @@ import org.springframework.cloud.config.monitor.GithubPropertyPathNotificationEx
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.Bean;
 
+/**
+ * 	 1. Run rabbitMP after checking rabbit container
+ *
+ * 	 	1-1. Check the docker is running or not
+ * 	 	lsof -nP -i4TCP | grep LISTEN|grep 15671
+ *
+ * 	 	1-2. Check exist rabbit container in docker
+ * 	 	docker ps -a|grep rabbit
+ *
+ * 	 	1-3. start rabbit container
+ * 	 	docker start rabbit
+ *
+ * 		1-4. If doesn't exist a rabbit container, run rabbit
+ * 	 	docker run -d --name rabbit -p 5672:5672 -p 15672:15672 rabbitmq:management
+ *
+ * 	 2. Run Config-service
+ */
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigApplication {
